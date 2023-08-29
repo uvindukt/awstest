7a56b51fbc4b227685777570f73bb36d33252c51
Uvindu Sanjana <uvindu.sanjana@gmail.com>
2023-08-29 13:27:36 +0530

    adding code

---
+++ src/main/java/com/nagarro/awstest/AwstestApplication.java
@@ -0,0 +1,14 @@
+package com.nagarro.awstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwstestApplication.class, args);
+		System.out.println("AWS test by Uvindu")
+	}
+
+}
